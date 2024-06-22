637998baa368fa3853770ef7de888800166ec858
Sathish R <91242874+Sathishr150601@users.noreply.github.com>
2024-06-22 17:46:16 +0530

    Routed Json data to page Component

--- src/components/Page/Home/Hero/index.tsx
+++ src/components/Page/Home/Hero/index.tsx
@@ -1,7 +1,29 @@
+import homeData from "@/data/home.json";
+
 const Hero: React.FC = () => {
+	const hero = homeData.hero[0];
+
 	return (
-		<div className="">
-			<div className=""></div>
+		<div className="" id="Hero">
+			<div className="">
+				<p className="PreHeading">
+					<span className="">{hero.preHeading1}</span>
+					<span className="">{hero.preHeading2}</span>
+				</p>
+			</div>
+			<div className="">
+				<h1 className="Title">
+					<span className="">{hero.title1}</span>
+					<span className="">{hero.title2}</span>
+					<span className="">{hero.title3}</span>
+				</h1>
+			</div>
+			<div className="">
+				<p className="Description">
+					<span className="">{hero.desc1}</span>
+					<span className="">{hero.desc2}</span>
+				</p>
+			</div>
 		</div>
 	);
 };
